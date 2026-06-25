e59b704f8bdf1c54253631cb51a9aec5309a6292
Rafael-Assanti <Rafael.assanti@gmail.com>
2026-06-24 21:31:34 -0300

    ativacao de pino 33

--- Testes de hardware/Controle_do_carro.py
+++ Testes de hardware/Controle_do_carro.py
@@ -1,14 +1,14 @@
 import Jetson.GPIO as GPIO
 import time
 
-# Tenta pelo nome do pino no chip em vez do board
-GPIO.setmode(GPIO.TEGRA_SOC)
-GPIO.setup("SPI1_MOSI", GPIO.OUT)  # equivale ao PWM0
+SERVO_PIN = 33
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
 
-pwm = GPIO.PWM("SPI1_MOSI", 50)
-pwm.start(7.5)
+pwm = GPIO.PWM(SERVO_PIN, 50)  # 50Hz = padrao servo
+pwm.start(7.5)                  # neutro (reto)
 
-print("Neutro")
+print("Neutro (reto)")
 time.sleep(2)
 
 print("Esquerda")
@@ -19,5 +19,14 @@ print("Neutro")
 pwm.ChangeDutyCycle(7.5)
 time.sleep(1)
 
+print("Direita")
+pwm.ChangeDutyCycle(10.0)
+time.sleep(1)
+
+print("Neutro")
+pwm.ChangeDutyCycle(7.5)
+time.sleep(1)
+
 pwm.stop()
 GPIO.cleanup()
+print("Teste concluido!")
